bdc1f11af85b25847a7754c0be25aee58ff11be1
Joeri Van Dooren <ure@rf.guru>
2023-04-23 16:53:35 +0200

    more engery savings

--- code.py
+++ code.py
@@ -1,3 +1,4 @@
+import alarm
 import time
 import board
 from rainbowio import colorwheel
@@ -5,11 +6,16 @@ import neopixel
 import rotaryio
 import digitalio
 
+pin_alarm = alarm.pin.PinAlarm(pin=board.GP18, value=False, pull=True)
+
 encoder = rotaryio.IncrementalEncoder(board.GP17, board.GP16)
 
 button = digitalio.DigitalInOut(board.GP18)
 button.direction = digitalio.Direction.INPUT
 button.pull = digitalio.Pull.UP
+
+while button.value is False:
+    time.sleep(0.001)
 
 pixel_pin = board.GP21
 num_pixels = 8
@@ -30,25 +36,23 @@ while True:
     current_position = encoder.position
     position_change = current_position - last_position
     if position_change > 0:
-        for _ in range(position_change):
+        for _ in range(position_change*2):
             if brightness < 0.99:
                 brightness=brightness+0.01
                 pixels.brightness=brightness
                 pixels.show()
-                print(str(brightness))
     elif position_change < 0:
-        for _ in range(-position_change):
+        for _ in range(-position_change*2):
             if brightness > 0.02:
                 brightness=brightness-0.01
                 pixels.brightness=brightness
                 pixels.show()
-                print(str(brightness))
     last_position = current_position
-    if not button.value and button_state is None:
-        button_state = "pressed"
     if button.value and button_state == "pressed":
-        print("Button pressed.")
         brightness=0.00
         pixels.brightness=brightness
         pixels.show()
         button_state = None
+        alarm.exit_and_deep_sleep_until_alarms(pin_alarm)
+    if not button.value and button_state is None:
+        button_state = "pressed"
